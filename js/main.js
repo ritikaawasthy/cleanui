69bcca71f795de8fa9fde59f866103f94f6b1468
ritikaawasthy <ritika.awasthy@gmail.com>
2022-02-16 20:41:37 +0530

    added rating

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// import {rating} from "/js/components/rating.js"
+//
+// const main=()=>{
+//
+//   const rateBtn= document.querySelector(".rating-responsive:nth-child(n)");
+//   rateBtn.addEventListener('click', rating(rateBtn));
+// }
+//
+// main();
