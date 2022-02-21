bfc9a3e3a261517c4f817b25fa72a0a98b64eb4d
ritikaawasthy <ritika.awasthy@gmail.com>
2022-02-21 12:12:53 +0530

    added rating comp

--- js/components/rating.js
+++ js/components/rating.js
@@ -1,18 +1,3 @@
-// const clickedRating=e=>{
-// console.log(e.target.class)
-//   if(typeof(e.target.class)=="undefined"){
-//     e.target.style.color= "var(--sec-col);"
-//     console.log("in undefines")
-//   }else{
-//     e.target.style.color= "var(--prim-col);"
-//     console.log("here")
-//   }
-// }
-//
-// export function rating(){
-//   return clickedRating
-// }
-
 document.getElementById('rating').addEventListener('click', function(event) {
   if (event.target.tagName.toLowerCase() != 'span') return;
 
--- js/main.js
+++
@@ -1,9 +0,0 @@
-// import {rating} from "/js/components/rating.js"
-//
-// const main=()=>{
-//
-//   const rateBtn= document.querySelector(".rating-responsive:nth-child(n)");
-//   rateBtn.addEventListener('click', rating(rateBtn));
-// }
-//
-// main();
