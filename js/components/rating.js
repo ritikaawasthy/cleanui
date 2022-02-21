8f8a383b30ca4c388b3902160780be5de603d57a
ritikaawasthy <ritika.awasthy@gmail.com>
2022-02-21 12:10:16 +0530

    added rating

--- js/components/rating.js
+++ js/components/rating.js
@@ -12,3 +12,16 @@
 // export function rating(){
 //   return clickedRating
 // }
+
+document.getElementById('rating').addEventListener('click', function(event) {
+  if (event.target.tagName.toLowerCase() != 'span') return;
+
+  if (event.target.classList.contains('checked')) {
+    event.target.classList.remove('checked');
+  } else {
+    Array.prototype.forEach.call(document.getElementsByClassName('checked'), function(el) {
+      el.classList.remove('checked');
+    });
+    event.target.classList.add('checked');
+  }
+});
