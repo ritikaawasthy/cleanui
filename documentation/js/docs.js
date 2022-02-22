8596e714cf3f400613b04062143ea65c668b366b
ritikaawasthy <ritika.awasthy@gmail.com>
2022-02-23 03:23:47 +0530

    docs page changes

---
+++ documentation/js/docs.js
@@ -0,0 +1,56 @@
+document.getElementById('usage').addEventListener('click', ()=>{
+  document.getElementById('doc-usage').scrollIntoView();
+})
+document.getElementById('alert').addEventListener('click', ()=>{
+  document.getElementById('doc-alert').scrollIntoView();
+})
+
+document.getElementById('avatar').addEventListener('click', ()=>{
+  document.getElementById('doc-avatar').scrollIntoView();
+})
+
+document.getElementById('badge').addEventListener('click', ()=>{
+  document.getElementById('doc-badge').scrollIntoView();
+})
+
+document.getElementById('card').addEventListener('click', ()=>{
+  document.getElementById('doc-card').scrollIntoView();
+})
+document.getElementById('button').addEventListener('click', ()=>{
+  document.getElementById('doc-button').scrollIntoView();
+})
+document.getElementById('input').addEventListener('click', ()=>{
+  document.getElementById('doc-input').scrollIntoView();
+})
+
+document.getElementById('text-utill').addEventListener('click', ()=>{
+  document.getElementById('doc-text').scrollIntoView();
+})
+
+document.getElementById('list').addEventListener('click', ()=>{
+  document.getElementById('doc-list').scrollIntoView();
+})
+
+document.getElementById('image').addEventListener('click', ()=>{
+  document.getElementById('doc-image').scrollIntoView();
+})
+
+document.getElementById('nav').addEventListener('click', ()=>{
+  document.getElementById('doc-nav').scrollIntoView();
+})
+
+document.getElementById('rate').addEventListener('click', ()=>{
+  document.getElementById('doc-rate').scrollIntoView();
+})
+
+document.getElementById('modal').addEventListener('click', ()=>{
+  document.getElementById('doc-modal').scrollIntoView();
+})
+
+document.getElementById('snb').addEventListener('click', ()=>{
+  document.getElementById('doc-snackbar').scrollIntoView();
+})
+
+document.getElementById('grids').addEventListener('click', ()=>{
+  document.getElementById('doc-grid').scrollIntoView();
+})
--- js/main.js
+++ js/main.js
@@ -1,12 +1,43 @@
-import {modal} from "/js/components/modal.js"
+// import {modal} from "/js/components/modal.js"
 
-const main=()=>{
   const modalOpenBtn= document.querySelector("#modal-open-btn");
   const modalCloseBtn= document.querySelector("#modal-close-btn");
+  const ratingBtn=   document.getElementById('rating');
+  const snackTrigger= document.querySelectorAll('.snackbar-trigger');
+  const snackBar= document.querySelector('.snackbar');
 
-  modalOpenBtn.addEventListener('click', modal());
-  modalCloseBtn.addEventListener("click", modal());
 
-}
+  snackTrigger.forEach(trigger=>{
+    console.log("clicken snackbar")
+    trigger.addEventListener('click', ()=>{
+      snackBar.classList.remove("hide");
+      setTimeout(()=>{
+        snackBar.classList.add("hide");
+      }, 3000);
+    });
+  });
 
-main();
+
+  document.getElementById('rating-id').addEventListener('click', function(event) {
+  if(event.target.classList.contains('checked')) {
+    event.target.classList.remove('checked');
+  } else {
+    Array.prototype.forEach.call(document.getElementsByClassName('checked'), function(el) {
+      el.classList.remove('checked');
+    });
+    event.target.classList.add('checked');
+  }
+});
+
+  const modalClicked=()=>{
+    const modalContainer= document.querySelector(".modal-container");
+    if (! (modalContainer.style.display)){
+      modalContainer.style.display="flex";
+    }else{
+      modalContainer.style.display=null;
+    }
+  }
+
+
+  modalOpenBtn.addEventListener('click', ()=>modalClicked());
+  modalCloseBtn.addEventListener("click", ()=>modalClicked());
